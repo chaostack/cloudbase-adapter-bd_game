a2324212cf684965b80cf6a44083402118c8d1c5
charming-wolf <c.witcher.w@gmail.com>
2019-12-16 19:01:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+## @cloudbase/adapter-bd_game
+
+[![NPM Version](https://img.shields.io/npm/v/@cloudbase/adapter-bd_game.svg?style=flat)](https://www.npmjs.com/package/@cloudbase/adapter-bd_game)
+[![](https://img.shields.io/npm/dt/@cloudbase/adapter-bd_game.svg)](https://www.npmjs.com/package/@cloudbase/adapter-bd_game)
+
+tcb-js-sdk百度小游戏适配器
+
+## 安装
+```bash
+npm i @cloudbase/adapter-bd_game -S
+```
+
+## 使用
+### ES Module
+```javascript
+import tcb from 'tsb-js-sdk';
+import adapterForWxMp from '@cloudbase/adapter-bd_game';
+
+// 以下两种方式二选一
+// 1.单参数传入
+tcb.useAdapters(adapterForWxMp);
+// 2.数组形式传参
+tcb.useAdapters([adapterForWxMp]);
+// adapter必须在init之前传入
+tcb.init();
+```
+
+### CommonJS
+```javascript
+const tcb = require('tsb-js-sdk');
+const adapterForWxMp = require('@cloudbase/adapter-bd_game');
+
+// 以下两种方式二选一
+// 1.单参数传入
+tcb.useAdapters(adapterForWxMp.adapter);
+// 2.数组形式传参
+tcb.useAdapters([adapterForWxMp.adapter]);
+// adapter必须在init之前传入
+tcb.init();
+```
---
+++ src/index.ts
@@ -0,0 +1,208 @@
+import {
+  AbstractSDKRequest,
+  IRequestOptions,
+  IUploadRequestOptions,
+  StorageInterface,
+  WebSocketInterface,
+  WebSocketContructor,
+  SDKAdapterInterface,
+  StorageType,
+  formatUrl
+} from '@cloudbase/adapter-interface';
+declare const swan;
+
+function isMatch(): boolean {
+  if (typeof swan === 'undefined') {
+    return false;
+  }
+  if (!swan.onHide) {
+    return false;
+  }
+  if (!swan.offHide) {
+    return false;
+  }
+  if (!swan.onShow) {
+    return false;
+  }
+  if (!swan.offShow) {
+    return false;
+  }
+  if (!swan.getSystemInfoSync) {
+    return false;
+  }
+  if (!swan.getStorageSync) {
+    return false;
+  }
+  if (!swan.setStorageSync) {
+    return false;
+  }
+  if (!swan.connectSocket) {
+    return false;
+  }
+  if (!swan.request) {
+    return false;
+  }
+
+  try {
+    if (!swan.getSystemInfoSync()) {
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+}
+
+class BdRequest extends AbstractSDKRequest {
+  constructor(){
+    super();
+  }
+  post(options: IRequestOptions) {
+    const { url, data, headers } = options;
+    return new Promise((resolve, reject) => {
+      swan.request({
+        url: formatUrl('https:', url),
+        data,
+        method: 'POST',
+        header: headers,
+        success(res) {
+          resolve(res);
+        },
+        fail(err) {
+          reject(err);
+        }
+      });
+    });
+  }
+  upload(options: IUploadRequestOptions) {
+    return new Promise(resolve => {
+      const { url, file, data, headers } = options;
+      swan.uploadFile({
+        url: formatUrl('https:', url),
+        filePath: file,
+        // 固定字段
+        name: 'file',
+        header: headers,
+        formData: { ...data, file },
+        success(res) {
+          const result = {
+            statusCode: res.statusCode,
+            data: res.data || {}
+          };
+          // 200转化为201（如果指定）
+          if (res.statusCode === 200 && data.success_action_status) {
+            result.statusCode = parseInt(data.success_action_status, 10);
+          }
+          resolve(result);
+        },
+        fail(err) {
+          resolve(err);
+        }
+      });
+    });
+  }
+  download(options: IRequestOptions) {
+    const { url, headers } = options;
+    return new Promise((resolve, reject) => {
+      swan.downloadFile({
+        url: formatUrl('https:', url),
+        header: headers,
+        success(res) {
+          if (res.statusCode === 200 && res.tempFilePath) {
+            // 由于涉及权限问题，只返回临时链接不保存到设备
+            resolve({
+              statusCode: 200,
+              tempFilePath: res.tempFilePath
+            });
+          } else {
+            resolve(res);
+          }
+        },
+        fail(err) {
+          reject(err);
+        }
+      });
+    });
+  }
+}
+const bdMpStorage: StorageInterface = {
+  setItem(key: string, value: any) {
+    swan.setStorageSync(key, value);
+  },
+  getItem(key: string): any {
+    return swan.getStorageSync(key);
+  },
+  removeItem(key: string) {
+    swan.removeStorageSync(key);
+  },
+  clear() {
+    swan.clearStorageSync();
+  }
+};
+
+class BdMpWebSocket {
+  constructor(url: string, options: object = {}) {
+    const READY_STATE = {
+      CONNECTING: 0,
+      OPEN: 1,
+      CLOSING: 2,
+      CLOSED: 3,
+    };
+    let readyState = READY_STATE.CONNECTING;
+
+    const ws = swan.connectSocket({
+      url,
+      ...options
+    });
+    const socketTask: WebSocketInterface = {
+      set onopen(cb) {
+        ws.onOpen(e => {
+          readyState = READY_STATE.OPEN;
+          cb && cb(e);
+        });
+      },
+      set onmessage(cb) {
+        ws.onMessage(cb);
+      },
+      set onclose(cb) {
+        ws.onClose(e => {
+          readyState = READY_STATE.CLOSED;
+          cb && cb(e);
+        });
+      },
+      set onerror(cb) {
+        ws.onError(cb);
+      },
+      send: (data) => ws.send({ data }),
+      close: (code?: number, reason?: string) => ws.close({ code, reason }),
+      get readyState() {
+        return readyState;
+      },
+      CONNECTING: READY_STATE.CONNECTING,
+      OPEN: READY_STATE.OPEN,
+      CLOSING: READY_STATE.CLOSING,
+      CLOSED: READY_STATE.CLOSED
+    };
+    return socketTask;
+  }
+}
+function genAdapter() {
+  // 小程序无sessionStorage
+  const adapter: SDKAdapterInterface = {
+    root: {},
+    reqClass: BdRequest,
+    wsClass: BdMpWebSocket as WebSocketContructor,
+    localStorage: bdMpStorage,
+    primaryStorage: StorageType.local
+  };
+  return adapter;
+}
+const adapter = { 
+  genAdapter, 
+  isMatch,
+  runtime: 'bd_game'
+};
+
+export {adapter};
+export default adapter;
